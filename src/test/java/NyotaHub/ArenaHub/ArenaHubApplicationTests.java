4ee1fb7648e7c92af2d0db9c0e7e5ab01f1f4629
elninja336 <abdillahalikh@gmail.com>
2025-08-07 09:18:50 +0300

    my first commit

---
+++ src/test/java/NyotaHub/ArenaHub/ArenaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package NyotaHub.ArenaHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArenaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
